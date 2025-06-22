8cd794f91e7e7442f312342181204c54b9ea0844
Facundo <facumolinacorujo@gmail.com>
2025-06-22 20:34:07 -0300

    funciona

--- src/main/java/MolinaCorujo/main/Main.java
+++ src/main/java/MolinaCorujo/main/Main.java
@@ -1,4 +1,15 @@
 package MolinaCorujo.main;
 
+import MolinaCorujo.framework.MenuFramework;
+
 public class Main {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Uso: java -jar tu_aplicacion.jar <ruta_archivo_configuracion>");
+            return;
+        }
+        String configFile = args[0];
+        MenuFramework framework = new MenuFramework(configFile);
+        framework.iniciar();
+    }
 }
--- src/main/java/MolinaCorujo/utilizacion/AccionUno.java
+++ src/main/java/MolinaCorujo/utilizacion/AccionUno.java
@@ -1,4 +1,25 @@
 package MolinaCorujo.utilizacion;
 
-public class AccionUno {
+import MolinaCorujo.framework.Accion;
+
+public class AccionUno implements Accion {
+    @Override
+    public void ejecutar() {
+        System.out.println("Ejecutando AccionUno: ¡Trayendo los tweets de José!");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Override
+    public String nombreItemMenu() {
+        return "Accion 1";
+    }
+
+    @Override
+    public String descripcionItemMenu() {
+        return "Esto es para traer los tweets de José...";
+    }
 }
